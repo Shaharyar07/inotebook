5d66a3b5f203d2672eeb433b9327b8bf8909eeac
shaharyar <shaharyar.tariq248@gmail.com>
2022-02-26 17:10:48 +0500

    final code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import About from "./Components/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AlertComp from "./Components/AlertComp";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import React from "react";
+
+
 
 function App() {
+  
+  const [alert, setAlert] = React.useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <AlertComp alert={alert} />
+        <div className="container border shadow px-5 bg-light">
+          <Switch>
+            <Route exact path="/">
+              <Home showAlert={showAlert} />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/signup">
+              <Signup showAlert={showAlert} />
+            </Route>
+            <Route exact path="/login">
+              <Login showAlert={showAlert} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
